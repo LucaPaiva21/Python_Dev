37dd9c866b20c620b2b0f4c5ba4fc55aaba2fb77
Luca Paiva <leandrolluca@hotmail.com>
2023-07-31 11:46:11 -0300

    Colocando explicações

---
+++ Python_Explicacoes/Explicando_Python.py
@@ -0,0 +1,29 @@
+'''
+Resumo sobre a linguagem de Python3
+'''
+#Python é:
+
+#Linguagem de propósito geral
+#Fácil e intuitivo
+#Multiplataforma
+#Baterias incluidas(muita coisa instalada)
+#Código aberto
+#Orientada a objeto
+#Muitas bibliotecas
+
+#Principais áreas
+
+#Inteligência artificial
+#Biotecnologia
+#Computação 3d
+
+#Quem usa python?:
+
+#Zope
+#Air camada
+#Bittorrent
+#Rede Globo
+#Google
+#Youtube
+#Nasa
+#Industrial Light e Magic
---
+++ Python_Explicacoes/Tipos_primitivos_e_saidas_de_dados.py
@@ -0,0 +1,23 @@
+'''
+Existe 4 tipos primitivos padrôes na linguagem python
+'''
+int # Números inteiros = 7, -4, 0, 9875
+float # Números reais = 7.0, -4.0, 0.0, 9875.0
+bool # Valores booleanos = True, False
+str # Texto = "Texto"
+
+'''
+Saída de dados
+'''
+
+# O mais padrão é
+nome = "Luca"
+print("Olá, sei que seu nome é:", nome)
+
+# A segunda forma é
+nome = "João"
+print("Olá, eu sei que seu nome é {}".format(nome))
+
+# A terceira e aque eu acho a mais simples é
+nome = "Maria"
+print(f"Olá, eu sei que seu nome é {nome}")
