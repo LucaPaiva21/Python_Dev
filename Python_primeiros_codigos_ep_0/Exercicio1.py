42926685ff9403b3d06cb4f7a9c3dbc0ac992201
Luca Paiva <leandrolluca@hotmail.com>
2023-07-31 11:38:17 -0300

    Corrigindo

--- Python_primeiros_codigos_ep_0/Exercicio1.py
+++
@@ -1,8 +0,0 @@
-'''
-Crie um script python que leia o nome de uma pessoa e mostre uma mensagem
-de boas-vindas de acordo com o valor digitado.
-'''
-
-nome_usuario = input("Digite seu nome: ")
-
-print(f"Olá {nome_usuario}, seja bem-vindo ao mundo Python!")
